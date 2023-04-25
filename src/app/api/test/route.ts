3fa271d8d3081d0874961a435c539d64be098cd6
“ale” <ale@777part.com>
2023-04-25 15:12:39 -0700

    test

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -2,5 +2,5 @@ import {NextResponse} from "next/server";
 import testjson from './test.json';
 
 export async function GET(request: Request) {
-    return new  NextResponse.json(testjson)
+    return NextResponse.json(testjson)
 }
